1020d92a1a1c8796785707d753b0c60998681d8a
RAVI8054 <ravi.jsd@hotmail.com>
2024-07-15 20:36:40 +0530

    change

--- src/Componets/sideNavbar/Side-navbar.jsx
+++ src/Componets/sideNavbar/Side-navbar.jsx
@@ -10,33 +10,34 @@ const Sidebar=(props)=>{
                   console.log(props)
                 return(
     <>
-        <div className="p-[150px]">
+     <div className="px-10 py-5 w-64 h-full bg-slate-600">
+        <div>
             <a href="https://iotcom.io/" target="_blank">
             <img src="https://monitoring.iotcom.io/Frame 66.png">
             </img></a>
         </div>
         <div>
-           <div>
+           <div className="flex text-xl font-semibold mt-10">
              <HiDesktopComputer />
              <span>Monitoring</span>
            </div>
-           <div>
+           <div className="flex text-xl font-semibold my-5">
              <FaShieldAlt />
              <span>Events</span>
            </div>
-           <div>
+           <div className="flex text-xl font-semibold my-5">
              <FaChartLine />
              <span>Status pages</span>
             </div>
-           <div>
+           <div className="flex text-xl font-semibold my-5">
              <FaTrash />
              <span>Trash</span>
            </div>
-           <div>
+           <div className="flex text-xl font-semibold my-5">
             <FaUser />
             <span>Team members</span>
            </div>
-           <div>
+           <div className="flex text-xl font-semibold my-5">
              <FaFireAlt />
              <span>Integration</span>
              </div>
@@ -46,6 +47,7 @@ const Sidebar=(props)=>{
           <img src="https://gravatar.com/avatar/4474ca42d303761c2901fa819c4f2547">
           </img>
         <span>shiv</span>
+        </div>
         </div>
     </>
   )}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Sidebar from  './Componets/side navbar/Side-navbar';
+import Sidebar from  './Componets/sideNavbar/Side-navbar';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <>
         <Sidebar></Sidebar>
+  </>
   </React.StrictMode>
 );
 
