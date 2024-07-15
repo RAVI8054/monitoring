692583fbbfceba592d416a670bf25a01358f126a
RAVI KUMAR <ravi.jsd@hotmail.com>
2024-07-15 19:40:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Sidebar from  './Componets/side navbar/Side-navbar';
+import Sidebar from  './Componets/sideNavbar/Side-navbar';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
